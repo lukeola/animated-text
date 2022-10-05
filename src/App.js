c665d8dbe0269c0bd50e1062e80a252b677df886
Luke Olawale <lukeolawale4@gmail.com>
2022-10-05 04:38:08 +0100

    animated text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import './hero.png';
+import Typewriter from 'typewriter-effect';
+ 
+  
+  const App = () => {
+    return (
+      <div className='animated__text'>
+      <Typewriter
+      onInit={(typewriter) => {
+        
+        typewriter.typeString('Hello')
+          .callFunction(() => {
+            // console.log('String typed out!');
+          })
+          .pauseFor(2500)
+          .deleteAll()
+          .callFunction(() => {
+            console.log('All strings were deleted');
+          })
+          .start();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          typewriter.typeString("I'm Luke Olawale")
+          .callFunction(() => {
+            // console.log('String typed out!');
+          })
+          .pauseFor(2500)
+          .deleteAll()
+          .callFunction(() => {
+            console.log('All strings were deleted');
+          })
+          .start();
+          
+          typewriter.typeString("A Full-Stack Developer")
+          .callFunction(() => {
+            // console.log('String typed out!');
+          })
+          .pauseFor(2500)
+          .deleteAll()
+          .callFunction(() => {
+            console.log('All strings were deleted');
+          })
+          .start();
+
+       
+      }}
+    />
     </div>
-  );
-}
+    )}
+    
+export default App
 
-export default App;
